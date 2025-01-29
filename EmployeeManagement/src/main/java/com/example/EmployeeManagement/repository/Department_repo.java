c715e057af87844c447c04d8c3397113d703601c
Rokiyashaik <rokiyashaik14@gmail.com>
2025-01-29 23:35:00 +0530

    deleted the file

--- EmployeeManagement/src/main/java/com/example/EmployeeManagement/repository/Department_repo.java
+++
@@ -1,8 +0,0 @@
-package com.example.EmployeeManagement.repository;
-
-import com.example.EmployeeManagement.model.Department_entity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Department_repo extends JpaRepository<Department_entity,Integer> {
-
-}
--- EmployeeManagement/src/main/java/com/example/EmployeeManagement/repository/Employee_repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.EmployeeManagement.repository;
-
-import com.example.EmployeeManagement.model.Employee_entity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Employee_repo extends JpaRepository<Employee_entity, Integer> {
-
-
-}
